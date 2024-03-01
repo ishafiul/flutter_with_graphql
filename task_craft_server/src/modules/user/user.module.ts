15f4d1624a7ff36ad69c84c097334ca28040d8ad
ishaf <shafiulislam20@gmail.com>
2024-03-01 12:07:51 +0600

    refactor(auth): di for mongoose schema models

--- task_craft_server/src/modules/user/user.module.ts
+++ task_craft_server/src/modules/user/user.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
+import { UserModel } from './entities/user.entity';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const MongooseModels = MongooseModule.forFeature([UserModel]);
 
 @Module({
   providers: [UserResolver, UserService],
+  exports: [UserService],
+  imports: [MongooseModels],
 })
 export class UserModule {}
