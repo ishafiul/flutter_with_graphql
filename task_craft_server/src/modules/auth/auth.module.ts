891d018d86105d86394a56b1b079a854ca760aad
ishaf <shafiulislam20@gmail.com>
2024-03-01 12:08:37 +0600

    refactor(auth): di for mongoose schema models for user service

--- task_craft_server/src/modules/auth/auth.module.ts
+++ task_craft_server/src/modules/auth/auth.module.ts
@@ -3,14 +3,21 @@ import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from '../../guards/jwt.strategy';
+import { UserService } from '../user/user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModel } from '../user/entities/user.entity';
+import { OtpModel } from './schema/otp.schema';
+
+const MongooseModels = MongooseModule.forFeature([UserModel, OtpModel]);
 
 @Module({
-  providers: [AuthResolver, AuthService, JwtStrategy],
+  providers: [AuthResolver, AuthService, JwtStrategy, UserService],
   imports: [
     JwtModule.register({
       secret: process.env.JWT_SECRECT,
       signOptions: { expiresIn: '7d' },
     }),
+    MongooseModels,
   ],
   exports: [AuthService, JwtStrategy, JwtModule],
 })
