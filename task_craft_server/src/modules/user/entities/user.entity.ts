d060ca95bc5862e321aff9a69efe7706b4eef50c
ishaf <shafiulislam20@gmail.com>
2024-03-01 12:05:43 +0600

    refactor: merged user schema with user entity

--- task_craft_server/src/modules/user/entities/user.entity.ts
+++ task_craft_server/src/modules/user/entities/user.entity.ts
@@ -1,7 +1,30 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+
+export type UserDocument = User & Document;
 
 @ObjectType()
+@Schema({
+  strict: false,
+  timestamps: true,
+})
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String, {})
+  _id: MongooseSchema.Types.ObjectId;
+  @Field()
+  @Prop({ type: String, isRequired: false })
+  firstName?: string;
+  @Field()
+  @Prop({ type: String, isRequired: false })
+  lastName?: string;
+  @Field()
+  @Prop({ type: String, isRequired: true })
+  email: string;
+  @Prop({ type: String, isRequired: true })
+  deviceUuId: string;
 }
+
+const schema = SchemaFactory.createForClass(User);
+
+export const UserModel = { name: User.name, schema };
