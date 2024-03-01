d6c7941e4a0bc86db1791845731c851500594c8c
ishaf <shafiulislam20@gmail.com>
2024-03-01 20:17:27 +0600

    feat(auth): request otp auth service

--- task_craft_server/src/modules/auth/auth.resolver.ts
+++ task_craft_server/src/modules/auth/auth.resolver.ts
@@ -5,6 +5,9 @@ import { CreateDeviceUuidInput } from './dto/create-device-uuid.input';
 import { RequestOtpInput } from './dto/request-otp.input';
 
 import { DeviceUuId } from './entities/device-uuid.entity';
+import { User } from '../user/entities/user.entity';
+import { VerifyOtpInput } from './dto/vreify-otp.input';
+import { TokenEntity } from './entities/token.entity';
 
 @Resolver(() => Auth)
 export class AuthResolver {
@@ -17,7 +20,7 @@ export class AuthResolver {
     return this.authService.createDeviceUuid(createDeviceUuidInput);
   }
 
-  @Mutation(() => Auth)
+  @Mutation(() => User)
   reqOtp(@Args('requestOtpInput') requestOtpInput: RequestOtpInput) {
     return this.authService.reqOtp(requestOtpInput);
   }
--- task_craft_server/src/modules/auth/auth.service.ts
+++ task_craft_server/src/modules/auth/auth.service.ts
@@ -7,10 +7,61 @@ import { UserService } from '../user/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly mailerService: MailerService,
+    private readonly jwtService: JwtService,
+    @InjectModel(OtpModel.name)
+    private readonly otpModel: Model<OtpDocument>,
+  ) {}
 
   async reqOtp(requestOtpInput: RequestOtpInput) {
-    await this.userService.findOne({ email: requestOtpInput.email });
+    const DeviceUuIdBytes = AES.decrypt(
+      requestOtpInput.deviceUuid,
+      process.env.HASH_KEY!,
+    );
+    const isDeviceUuId = DeviceUuIdBytes.toString(enc.Utf8);
+
+    if (!isDeviceUuId) {
+      throw new HttpException('Device uuid invalid ', HttpStatus.BAD_REQUEST);
+    }
+    const oldOtp = await this.otpModel.findOne({
+      email: requestOtpInput.email,
+      deviceUuId: requestOtpInput.deviceUuid,
+    });
+    if (oldOtp) {
+      await this.otpModel.deleteOne({
+        email: requestOtpInput.email,
+        deviceUuId: requestOtpInput.deviceUuid,
+      });
+    }
+    let user: User;
+    user = await this.userService.findOne({
+      email: requestOtpInput.email,
+    });
+    if (!user) {
+      user = await this.userService.create({
+        email: requestOtpInput.email,
+        deviceUuId: requestOtpInput.deviceUuid,
+      });
+    }
+
+    const otp = generateOtp(5);
+
+    await this.otpModel.create({
+      email: requestOtpInput.email,
+      otp,
+      deviceUuId: requestOtpInput.deviceUuid,
+    });
+    await this.mailerService.sendMail({
+      to: requestOtpInput.email,
+      subject: 'OTP',
+      template: 'sendOtp',
+      context: {
+        otp: otp,
+      },
+    });
+    return user;
   }
 
   createDeviceUuid(createDeviceUuidInput: CreateDeviceUuidInput): DeviceUuId {
