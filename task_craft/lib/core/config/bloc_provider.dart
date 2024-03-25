6785c5669116b1394349f7b427f55dafa47acc48
ishaf <shafiulislam20@gmail.com>
2024-03-25 21:12:33 +0600

    feat: verify otp cubit provider

--- task_craft/lib/core/config/bloc_provider.dart
+++ task_craft/lib/core/config/bloc_provider.dart
@@ -5,6 +5,7 @@ import 'package:task_craft/app/app.dart';
 import 'package:task_craft/core/snackbar/cubit/snack_bar_cubit.dart';
 import 'package:task_craft/module/auth/data/repositories/auth_repository.dart';
 import 'package:task_craft/module/auth/domain/cubit/request_otp/request_otp_cubit.dart';
+import 'package:task_craft/module/auth/domain/cubit/verify_otp/verify_otp_cubit.dart';
 
 /// [Provider] is a [Singleton] that will provide [List] of [BlocProvider].
 /// need to provide [BlocProvider] for those `bloc` or `cubit` that will be available
@@ -20,6 +21,11 @@ class Provider {
     BlocProvider<RequestOtpCubit>(
       create: (BuildContext context) =>
           RequestOtpCubit(repository: AuthRepository()),
+    ),
+
+    BlocProvider<VerifyOtpCubit>(
+      create: (BuildContext context) =>
+          VerifyOtpCubit(repository: AuthRepository()),
     ),
   ];
 }
