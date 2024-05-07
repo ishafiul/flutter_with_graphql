e3dd084d5c1f99b7787f7a7e385b9b1d4f64ec37
ishaf <shafiulislam20@gmail.com>
2024-05-07 20:53:44 +0600

    feat(user): user schema and gql field updated

--- task_craft_server/src/modules/user/entities/user.entity.ts
+++ task_craft_server/src/modules/user/entities/user.entity.ts
@@ -1,6 +1,8 @@
 import { ObjectType, Field } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Schema as MongooseSchema } from 'mongoose';
+import { ERole } from '../../../common/enums/user';
+import { IsEnum } from 'class-validator';
 
 export type UserDocument = User & Document;
 
@@ -10,17 +12,32 @@ export type UserDocument = User & Document;
   timestamps: true,
 })
 export class User {
-  @Field(() => String, {})
+  @Field(() => String, { description: 'user uuid' })
   _id: MongooseSchema.Types.ObjectId;
-  @Field()
+  @Field({ description: 'user first name. its not required and can be null' })
   @Prop({ type: String, isRequired: false })
   firstName?: string;
-  @Field()
+  @Field({ description: 'user last name. its not required and can be null' })
   @Prop({ type: String, isRequired: false })
   lastName?: string;
-  @Field()
-  @Prop({ type: String, isRequired: true })
+  @Field({
+    description:
+      'user profile picture (public). its not required and can be null',
+  })
+  @Prop({
+    type: String,
+    isRequired: false,
+  })
+  profilePicture?: string;
+  @Field({ description: 'user registered email' })
+  @Prop({
+    type: String,
+    isRequired: true,
+  })
   email: string;
+  @Prop({ type: String, enum: ERole, default: ERole.USER })
+  @IsEnum(ERole)
+  role: string;
 }
 
 const schema = SchemaFactory.createForClass(User);
