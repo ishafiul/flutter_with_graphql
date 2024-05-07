9a5fe3e388d8ec5f40b94c057d5c5f4893c1eb72
ishaf <shafiulislam20@gmail.com>
2024-05-07 20:45:04 +0600

    feat(user): Add ERole, EAccessType, and EFilePermission enums

---
+++ task_craft_server/src/common/enums/user.ts
@@ -0,0 +1,17 @@
+export enum ERole {
+  USER = 'USER',
+  ADMIN = 'ADMIN',
+  SUPER_ADMIN = 'SUPER_ADMIN',
+}
+
+export enum EAccessType {
+  PUBLIC = 'PUBLIC',
+  PRIVATE = 'PRIVATE',
+}
+
+export enum EFilePermission {
+  READ = 'READ',
+  WRITE = 'WRITE',
+  DELETE = 'DELETE',
+  SHARE = 'SHARE',
+}
