adacb6b5659abcd3fb1afe626db834da07904faa
ishaf <shafiulislam20@gmail.com>
2024-05-20 21:03:33 +0600

    refactor(todo): date timeline
    updated set state with valueNotifier fo performance and reduce unnecessary widget re build

--- task_craft/lib/module/todo/presentation/widgets/date_timeline.dart
+++ task_craft/lib/module/todo/presentation/widgets/date_timeline.dart
@@ -8,6 +8,86 @@ import 'package:task_craft/core/config/colors.dart';
 import 'package:task_craft/core/utils/extention.dart';
 import 'package:task_craft/core/widgets/devider/divider.dart';
 
+class DateTile extends StatefulWidget {
+  final DateTime date;
+  final bool isSelected;
+  final bool isToday;
+  final VoidCallback onTap;
+
+  const DateTile({
+    super.key,
+    required this.date,
+    required this.isSelected,
+    required this.isToday,
+    required this.onTap,
+  });
+
+  @override
+  _DateTileState createState() => _DateTileState();
+}
+
+class _DateTileState extends State<DateTile> {
+  @override
+  Widget build(BuildContext context) {
+    final String dayNumber = DateFormat('d').format(widget.date);
+    final String dayName = DateFormat('E').format(widget.date);
+
+    return Container(
+      key: GlobalObjectKey(widget.date),
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      height: 56,
+      width: 56.0,
+      decoration: BoxDecoration(
+        color: widget.isSelected ? CColor.primary : Colors.white,
+        border: widget.isToday ? Border.all(color: CColor.primary) : Border.all(color: CColor.border),
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: widget.onTap,
+          child: badges.Badge(
+            badgeStyle: badges.BadgeStyle(
+              shape: badges.BadgeShape.square,
+              badgeColor: CColor.primary.shade200,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(4),
+                topRight: Radius.circular(4),
+                bottomLeft: Radius.circular(4),
+              ),
+              elevation: 0,
+            ),
+            position: badges.BadgePosition.bottomEnd(bottom: -1, end: -8),
+            badgeContent: const Text(
+              '00',
+              style: TextStyle(color: Colors.white),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    dayNumber,
+                    style: context.textTheme.bodyMedium?.copyWith(
+                      color: widget.isSelected ? Colors.white : CColor.text,
+                    ),
+                  ),
+                  Text(
+                    dayName,
+                    style: context.textTheme.bodyMedium?.copyWith(
+                      color: CColor.light,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class DateTimeLine extends StatefulWidget {
   const DateTimeLine({super.key, this.controller, this.visibleDate});
 
@@ -18,50 +98,43 @@ class DateTimeLine extends StatefulWidget {
   @override
   State<DateTimeLine> createState() => _DateTimeLineState();
 }
-
 class _DateTimeLineState extends State<DateTimeLine> {
   final ScrollController _controller = ScrollController();
-  DateTime? _currentDate;
+  final ValueNotifier<DateTime?> _currentDateNotifier = ValueNotifier<DateTime?>(null);
 
-  // a function that will take a date and return a new date with only day month year
   DateTime _getOnlyDate(DateTime date) {
     return DateTime(date.year, date.month, date.day);
   }
 
   @override
   void initState() {
-    _currentDate = _getOnlyDate(DateTime.now());
+    super.initState();
+    _currentDateNotifier.value = _getOnlyDate(DateTime.now());
     widget.controller?.setDatePickerState(this);
 
-    // if widget rendered then animate to today date
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       widget.controller?.animateToDate(DateTime.now());
     });
     _controller.addListener(_scrollListener);
-    super.initState();
   }
 
   void _scrollListener() {
     if (_controller.hasClients) {
-      final double centerOffset = _controller.offset +
-          (MediaQuery.of(context).size.width / 2);
+      final double centerOffset = _controller.offset + (MediaQuery.of(context).size.width / 2);
       final DateTime visibleDate = widget.controller!.calculateDateFromOffset(centerOffset);
       widget.visibleDate?.call(visibleDate);
     }
   }
 
   void _updateCurrentDate(DateTime date) {
-    setState(() {
-      _currentDate = _getOnlyDate(date);
-      logger.d(_currentDate);
-    });
+    _currentDateNotifier.value = _getOnlyDate(date);
+    logger.d(_currentDateNotifier.value);
   }
 
   @override
   Widget build(BuildContext context) {
     final controller = AutoScrollController(
-      viewportBoundaryGetter: () =>
-          Rect.fromLTRB(0, 0, 0, MediaQuery.of(context).padding.bottom),
+      viewportBoundaryGetter: () => Rect.fromLTRB(0, 0, 0, MediaQuery.of(context).padding.bottom),
       axis: Axis.horizontal,
     );
 
@@ -82,110 +155,43 @@ class _DateTimeLineState extends State<DateTimeLine> {
             controller: _controller,
             scrollDirection: Axis.horizontal,
             itemBuilder: (context, index) {
-              final DateTime currentDate =
-                  DateTime(2024).add(Duration(days: index));
-
+              final DateTime currentDate = DateTime(2024).add(Duration(days: index));
               final curentDateOnly = _getOnlyDate(currentDate);
               final isToday = DateTime(
-                    currentDate.year,
-                    currentDate.month,
-                    currentDate.day,
-                  ) ==
-                  DateTime(
-                    DateTime.now().year,
-                    DateTime.now().month,
-                    DateTime.now().day,
-                  );
-              // Format the date as desired
-              final String dayNumber = DateFormat('d').format(currentDate);
-              final String dayName = DateFormat('E').format(currentDate);
-              // Return a ListTile or any other widget with the formatted date
+                currentDate.year, currentDate.month, currentDate.day,
+              ) == DateTime(
+                DateTime.now().year, DateTime.now().month, DateTime.now().day,
+              );
+
               return wrapScrollTag(
                 index: index,
-                child: Container(
-                  key: GlobalObjectKey(currentDate),
-                  margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                  height: 56,
-                  width: 56.0,
-                  decoration: BoxDecoration(
-                    color: _currentDate == curentDateOnly
-                        ? CColor.primary
-                        : Colors.white,
-                    border: isToday
-                        ? Border.all(color: CColor.primary)
-                        : Border.all(
-                            color: CColor.border,
-                          ),
-                  ),
-                  child: Material(
-                    color: Colors.transparent,
-                    child: InkWell(
+                child: ValueListenableBuilder<DateTime?>(
+                  valueListenable: _currentDateNotifier,
+                  builder: (context, selectedDate, _) {
+                    final isSelected = selectedDate == curentDateOnly;
+
+                    return DateTile(
+                      date: currentDate,
+                      isSelected: isSelected,
+                      isToday: isToday,
                       onTap: () {
                         Scrollable.ensureVisible(
                           GlobalObjectKey(currentDate).currentContext!,
                           duration: const Duration(milliseconds: 500),
-                          // duration for scrolling time
                           alignment: .5,
-                          // 0 mean, scroll to the top, 0.5 mean, half
                           curve: Curves.easeInOutCubic,
                         );
                         _updateCurrentDate(currentDate);
                       },
-                      child: badges.Badge(
-                        badgeStyle: badges.BadgeStyle(
-                          shape: badges.BadgeShape.square,
-                          badgeColor: CColor.primary.shade200,
-                          borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(
-                              4,
-                            ),
-                            topRight: Radius.circular(
-                              4,
-                            ),
-                            bottomLeft: Radius.circular(
-                              4,
-                            ),
-                          ),
-                          elevation: 0,
-                        ),
-                        position:
-                            badges.BadgePosition.bottomEnd(bottom: -1, end: -8),
-                        badgeContent: const Text(
-                          '00',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                dayNumber,
-                                style: context.textTheme.bodyMedium?.copyWith(
-                                  color: CColor.text,
-                                ),
-                              ),
-                              Text(
-                                dayName,
-                                style: context.textTheme.bodyMedium?.copyWith(
-                                  color: CColor.light,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
+                    );
+                  },
                 ),
               );
             },
           ),
         ),
         8.verticalSpace,
-        CDivider.base(
-          axis: Axis.horizontal,
-        ),
+        CDivider.base(axis: Axis.horizontal),
         8.verticalSpace,
       ],
     );
@@ -207,7 +213,7 @@ class DateTimelineController {
 
     // jump to the current Date
     _datePickerState!._controller.jumpTo(
-        _calculateDateOffset(_datePickerState?._currentDate ?? DateTime.now()));
+        _calculateDateOffset(_datePickerState?._currentDateNotifier.value  ?? DateTime.now()));
   }
 
   /// This function will animate the Timeline to the currently selected Date
@@ -217,9 +223,10 @@ class DateTimelineController {
 
     // animate to the current date
     _datePickerState!._controller.animateTo(
-        _calculateDateOffset(_datePickerState!._currentDate ?? DateTime.now()),
-        duration: const Duration(milliseconds: 200),
-        curve: Curves.easeInOut);
+      _calculateDateOffset(_datePickerState!._currentDateNotifier.value  ?? DateTime.now()),
+      duration: const Duration(milliseconds: 200),
+      curve: Curves.easeInOut,
+    );
   }
 
   /// This function will animate to any date that is passed as an argument
